d496d83479c20ded0918bddb748c4ac5d356d92b
blueback <bluebackpandey@gmail.com>
2024-11-24 18:19:46 +0530

    some cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@
 int main() {
   CircuitSolverSelfTest circuit_solver_self_test;
   circuit_solver_self_test.selfTest();
+
   //RaylibProbeSelfTest raylib_probe_self_test;
   //raylib_probe_self_test.selfTest();
-  CircuitAnimationDemoSelfTest circuit_animation_demo_self_test;
-  circuit_animation_demo_self_test.selfTest();
+
+  //CircuitAnimationDemoSelfTest circuit_animation_demo_self_test;
+  //circuit_animation_demo_self_test.selfTest();
 
   return 0;
 }
