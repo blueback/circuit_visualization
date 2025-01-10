c64b7d0019d6d11f46128f983b5f835b6ecef722
blueback <bluebackpandey@gmail.com>
2025-01-10 10:21:44 +0530

    finding queue families

--- experiments/vulkan_tutorial/src/main.cpp
+++ experiments/vulkan_tutorial/src/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <map>
+#include <optional>
 #include <vector>
 #include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
@@ -259,6 +260,15 @@ private:
     vkGetPhysicalDeviceProperties(device, &deviceProperties);
     vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
 
+    std::cout << "Checking Device \"" << deviceProperties.deviceName
+              << "\" Suitability ..." << std::endl;
+
+    QueueFamilyIndices indices = findQueueFamilies(device);
+
+    if (!indices.isComplete()) {
+      return false;
+    }
+
     if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
       if (deviceFeatures.geometryShader) {
         return true;
@@ -375,6 +385,39 @@ private:
       throw std::runtime_error(
           "failed to find a suitable GPU, not sufficient score!");
     }
+  }
+
+  struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+
+    bool isComplete() { return graphicsFamily.has_value(); }
+  };
+
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+    // Logic to find graphics queue family to populate struct with
+    // Assign index to queue families that could be found
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount,
+                                             nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount,
+                                             queueFamilies.data());
+
+    int i = 0;
+    for (const auto &queueFamily : queueFamilies) {
+      if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+        indices.graphicsFamily = i;
+      }
+
+      if (indices.isComplete()) {
+        break;
+      }
+      i++;
+    }
+
+    return indices;
   }
 
   void initVulkan() {
